472a40dbd4ffbc7435b254dee38b1d9b6621c2f3
Matt Lopez <matthew.lopez4011@gmail.com>
2019-10-22 15:20:48 -0500

    add logic to login servlet

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -5,23 +5,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "ViewProfileServlet", urlPatterns = "/login")
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/profile.jsp").forward(request, response);
+        request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         if (request.getMethod().equalsIgnoreCase("post")) {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
             if (username.equals("admin") && password.equals("password")) {
                 response.sendRedirect("/profile");
+            }else{
+                response.sendRedirect("/login");
             }
         }
-        request.setAttribute("password", password);
-        request.setAttribute("username", username);
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+//        request.setAttribute("password", password);
+//        request.setAttribute("username", username);
+//        request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 }
